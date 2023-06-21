09127099eef978057850b8e08edd4e65a6b275af
qjcoder <qjcoder@gmail.com>
2023-06-21 18:17:14 +0500

    register page UI

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import styles from "./register.module.css";
+import Button from "@/components/button/button";
+import Link from "next/link";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Create an Account</h1>
+      <h2>Please sign up to see the dashboard.</h2>
+      <form className={styles.form}>
+        <input placeholder="Username" type="text" className={styles.input} />
+        <input placeholder="Email" type="text" className={styles.input} />
+        <input placeholder="Password" type="text" className={styles.input} />
+
+        <button className={styles.button}>
+          <Link href="/dashboard/login">Register</Link>
+        </button>
+      </form>
+      <span> -OR-</span>
+      <Link href="/dashboard/login">Login with an existing account</Link>
+    </div>
+  );
 };
 
 export default Register;
