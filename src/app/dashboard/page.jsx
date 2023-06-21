9ce304f6f3a5ce0e67c63841aad68c297f395169
qjcoder <qjcoder@gmail.com>
2023-06-21 19:15:18 +0500

    dashboard ui

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,54 @@
-import React from "react";
 import styles from "./dashboard.module.css";
+import Image from "next/image";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.posts}>
+        <div className={styles.post}>
+          <div className={styles.imgContainer}>
+            <Image src="/illustration.png" alt="" width={200} height={100} />
+          </div>
+          <h2 className={styles.postTitle}>Navigating the Mobile Frontier:</h2>
+          <span className={styles.delete}>X</span>
+        </div>
+        <div className={styles.post}>
+          <div className={styles.imgContainer}>
+            <Image src="/illustration.png" alt="" width={200} height={100} />
+          </div>
+          <h2 className={styles.postTitle}>Navigating the Mobile Frontier:</h2>
+          <span className={styles.delete}>X</span>
+        </div>
+        <div className={styles.post}>
+          <div className={styles.imgContainer}>
+            <Image src="/illustration.png" alt="" width={200} height={100} />
+          </div>
+          <h2 className={styles.postTitle}>Navigating the Mobile Frontier:</h2>
+          <span className={styles.delete}>X</span>
+        </div>
+        <div className={styles.post}>
+          <div className={styles.imgContainer}>
+            <Image src="/illustration.png" alt="" width={200} height={100} />
+          </div>
+          <h2 className={styles.postTitle}>Navigating the Mobile Frontier:</h2>
+          <span className={styles.delete}>X</span>
+        </div>
+      </div>
+      <form className={styles.new}>
+        <h1>Add New Post</h1>
+        <input type="text" placeholder="Title" className={styles.input} />
+        <input type="text" placeholder="Desc" className={styles.input} />
+        <input type="text" placeholder="Image" className={styles.input} />
+        <textarea
+          placeholder="Content"
+          className={styles.textArea}
+          cols="30"
+          rows="10"
+        ></textarea>
+        <button className={styles.button}>Send</button>
+      </form>
+    </div>
+  );
 };
 
 export default Dashboard;
